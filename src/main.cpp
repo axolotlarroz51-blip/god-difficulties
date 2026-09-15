aad898650f224aefdfd5b1fbc674e05ba1f97035
axolotlarroz51-blip <axolotlarroz51@gmail.com>
2026-09-15 09:13:01 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/LevelCell.hpp>
 using namespace geode::prelude;
 
-class $modify(GodCell, LevelCell) {
-    void loadFromLevel(GJGameLevel* level) {
-        LevelCell::loadFromLevel(level);
-        log::info("GOD mod cargado para nivel: {}", level->m_levelName);
-    }
-};
+$on_mod(Loaded) {
+    log::info("God Difficulties loaded");
+}
