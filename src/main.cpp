f06ba23e566bb9be40ca52c47ec8b28c557772a1
axolotlarroz51-blip <axolotlarroz51@gmail.com>
2026-09-15 08:40:42 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@ using namespace geode::prelude;
 class $modify(GodCell, LevelCell) {
     void loadFromLevel(GJGameLevel* level) {
         LevelCell::loadFromLevel(level);
-        if (!level) return;
-        std::string name = level->m_levelName;
-        std::transform(name.begin(), name.end(), name.begin(), ::toupper);
-        if (name.find("[GOD]") == std::string::npos) return;
-        if (auto spr = this->getChildByType<GJDifficultySprite>(0)) {
-            spr->setTexture(CCTextureCache::sharedTextureCache()->addImage("god_difficulty.png"_spr, false));
-        }
+        log::info("GOD mod cargado para nivel: {}", level->m_levelName);
     }
 };
