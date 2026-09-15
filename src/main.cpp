17117c0d1d354924a0a96dc4e7fa412b29220526
axolotlarroz51-blip <axolotlarroz51@gmail.com>
2026-09-15 10:28:30 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 using namespace geode::prelude;
 #include <Geode/modify/GJDifficultySprite.hpp>
 
-class $modify(GJDifficultySprite) {
-    void updateDifficulty(int diff) {
-        GJDifficultySprite::updateDifficulty(diff);
-        
+class $modify(GODDiff, GJDifficultySprite) {
+    bool init(int diff, GJDifficultyName name, bool feature) {
+        if (!GJDifficultySprite::init(diff, name, feature)) return false;
+
         std::string file = "";
-        // Mapeamos tus 4 GOD faces
+        // Tus 4 caras GOD
         if (diff == 1) file = "1789487493184.png"_spr; // Peaceful rosita
-        if (diff == 2) file = "1789487508957.png"_spr; // GOD blanco con halo
-        if (diff == 3) file = "1789487486240.png"_spr; // IMPOSSIBLE rojo
-        if (diff == 4) file = "1789487503657.png"_spr; // ULTRA (la negrita)
-        if (diff == 5) file = "1789487508957.png"_spr; // Extra -> GOD
+        if (diff == 2) file = "1789487508957.png"_spr; // GOD
+        if (diff == 3) file = "1789487486240.png"_spr; // Impossible
+        if (diff == 4) file = "1789487503657.png"_spr; // Ultra / negra
+        if (diff == 5) file = "1789487508957.png"_spr;
 
         if (!file.empty()) {
             if (auto spr = this->getChildByType<CCSprite>(0)) {
@@ -20,9 +20,6 @@ class $modify(GJDifficultySprite) {
                 if (tex) spr->setTexture(tex);
             }
         }
+        return true;
     }
 };
-
-$on_mod(Loaded) {
-    log::info("GOD Difficulties by axolotl loaded!");
-}
